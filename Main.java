7a6936cafa444794e542e9ab6eccc23bc0afedb3
IgorFratczak <245804@edu.p.lodz.pl>
2025-03-12 09:45:58 +0100

    feature_Lab1_Zad4

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
-public class Main {
+
+interface IAritmeticsDiv{
+    double Division(double A, double B);
+}
+
+
+
+public class Main implements IAritmeticsDiv {
+    @override
+    public static double Division(double A, double B){
+        if (B == 0) return 0;
+        return A/B;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("MIVR, DevOps, romanbeznosenko");
         System.out.println("Andezion - Vlad");
