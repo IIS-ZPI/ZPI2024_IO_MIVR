5cc8b8f1825af1506b29485c539994f8b93a2df7
Andezion <245122@edu.p.lodz.pl>
2025-03-12 09:40:06 +0100

    Added math

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+interface IArithmeticsAdd
+{
+    double Addition(double A, double B);
+}
+
+public class Main implements IArithmeticsAdd
+{
+    double Addition(double A, double B)
+    {
+        return A + B;
+    }
+
+    public static void main(String[] args) 
+    {
         System.out.println("MIVR, DevOps, romanbeznosenko");
         System.out.println("Andezion - Vlad");
     }
