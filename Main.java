7de4f1ab38909a33dd0cb0ce129758c36d7cfe52
Andezion <245122@edu.p.lodz.pl>
2025-03-12 08:59:57 +0100

    Added myself

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("MIVR, DevOps, romanbeznosenko");
+        System.out.println("Andezion - Vlad");
     }
 }
