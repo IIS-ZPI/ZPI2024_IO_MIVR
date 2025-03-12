54e281bff2c81c4ac43b1e4d03a1120407b21a21
romanbeznosenko <245125@edu.p.lodz.pl>
2025-03-12 09:40:02 +0100

    Implemented IArithmeticsDiff

--- Main.java
+++ Main.java
@@ -1,4 +1,14 @@
-public class Main {
+interface IArithmeticsDiff
+{
+    double Difference(double A, double B);
+}
+
+public class Main implements IArithmeticsDiff{
+    double Difference(double A, double B)
+    {
+        return A - B;
+    }
+
     public static void main(String[] args) {
         System.out.println("MIVR, DevOps, romanbeznosenko");
         System.out.println("Andezion - Vlad");
