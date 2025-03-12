8261cbf9cb0040fdac7b6ddf202da5cf2d113d94
MichalC <166399171+MiszorES@users.noreply.github.com>
2025-03-12 09:42:32 +0100

    Implemented IAritheticsMult

--- Main.java
+++ Main.java
@@ -1,4 +1,14 @@
-public class Main {
+interface IArithmeticsMult
+{
+    double Multiplication(double A,double B);
+}
+
+public class Main implements IArithmeticsMult{
+    @Override
+    public double Multiplication(double A, double B) {
+        return A * B;
+    }
+
     public static void main(String[] args) {
         System.out.println("MIVR, DevOps, romanbeznosenko");
         System.out.println("Andezion - Vlad");
